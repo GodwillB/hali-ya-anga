1f8578142df61627f6376e53b5eca948069b53b9
GodwillB <iamgodwillb@gmail.com>
2020-10-10 05:05:38 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Weather from './app_component/weather.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
